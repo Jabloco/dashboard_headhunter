8f2ff6776c1628954b3db32190f7cb06473c0341
Andrey Butakov <imblackmamba13@yandex.ru>
2021-09-27 14:53:08 +0300

    Поправил оформление и requirments

--- flask_carcas/app/forms.py
+++ flask_carcas/app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    vacancy_name = StringField('Введи вакансию', validators=[DataRequired()])
-    submit = SubmitField('Искать')
+    vacancy_name = StringField("Введи вакансию", validators=[DataRequired()])
+    submit = SubmitField("Искать")
