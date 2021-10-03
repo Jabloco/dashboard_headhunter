ab52591529592b8dc9c79693d324962610a3637a
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-10-03 18:31:29 +0300

    Добавил модель area

--- flask_carcas/app/models.py
+++ flask_carcas/app/models.py
@@ -1,9 +1,12 @@
+from os import name
 from app import db
 
 class Area(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    hh_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
     def __repr__(self):
-        pass
+        return f'id: {self.id}, hh_id: {self.hh_id}, area_name: {self.name}'
 
 class KeySkill(db.Model):
     pass
