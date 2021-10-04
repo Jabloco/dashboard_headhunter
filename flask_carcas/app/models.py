a561c6765a8e225b01039e410659c1c6f4ab4e2f
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-10-04 10:03:35 +0300

    Добавил модель KeySkill и связующую таблицу

--- flask_carcas/app/models.py
+++ flask_carcas/app/models.py
@@ -1,25 +1,39 @@
 from os import name
 from app import db
 
+keyskill = db.Table('vacancy_skill',
+    db.Column('vacancy_id', db.Integer, db.ForeignKey('vacancy.id'), primary_key=True),
+    db.Column('keyskill_id', db.Integer, db.ForeignKey('keyskill.id'), primary_key=True)
+)
+
+
 class Area(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hh_id = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(120), nullable=False)
+
     def __repr__(self):
         return f'id: {self.id}, hh_id: {self.hh_id}, area_name: {self.name}'
 
+
 class KeySkill(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    keyskill_relation = db.relationship('Vacancy', secondary=keyskill)
+
     def __repr__(self):
-        pass
+        return f'id: {self.id}, keyskill_name: {self.name}'
+
 
 class Vacancy(db.Model):
     pass
+
     def __repr__(self):
         pass
 
+
 class Employer(db.Model):
     pass
+
     def __repr__(self):
         pass
-    
