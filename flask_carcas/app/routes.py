8e69064ec4e11ff80a6800ef0488ec35da6e60db
Andrey Butakov <imblackmamba13@yandex.ru>
2021-10-18 18:37:00 +0300

    Поправил имена функций в routes.py

--- flask_carcas/app/routes.py
+++ flask_carcas/app/routes.py
@@ -9,22 +9,23 @@ from app.dashboards import create_pie_dashboard, dash_link
 @app.route("/index")
 def index():
     page_text = "Привет!"
-    return render_template("index.html", title="Home", page_text=page_text)
+    return render_template("index.html", title="О проекте", page_text=page_text)
 
-@app.route("/pie_dash", methods=["GET"])
-def dash():
+@app.route("/keyskills")
+def keyskills():
+    page_text = "Ключевые навыки"
+    return render_template("index.html", title="Ключевые навыки", page_text=page_text)
+
+@app.route("/salary")
+def salary():
+    page_text = "Распределение зарплат"
+    return render_template("index.html", title="Распределение зарплат", page_text=page_text)
+
+@app.route("/vacancies", methods=["GET"])
+def vacancies():
     """При вводе даты передает значения в переменные date_from, date_to."""
     image = dash_link(create_pie_dashboard)
     date_from = request.args.get("date_from")
     date_to  = request.args.get("date_to")
     flash(f"Выбранная дата: c {date_from} до {date_to}.")
-    return render_template("pie_dash.html", image=image)
-
-@app.route("/vacancy", methods=["GET", "POST"])
-def vacancy():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f"Показана статистика по вакансии: {form.vacancy_name.data}.")
-        return redirect(url_for("dash"))
-    return render_template("vacancy_search.html", title="Dashboard", form=form)
-
+    return render_template("vacancies.html", image=image)
