4ecbb7727ea0f01273006f3286dd05c740adcdaf
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-10-25 16:16:42 +0300

    Удалил не используемые импорты. Добавил raise-ов

--- flask_carcas/app/routes.py
+++ flask_carcas/app/routes.py
@@ -1,27 +1,10 @@
 from datetime import date, datetime
-from flask.helpers import url_for
-from flask import render_template, flash, redirect, request
+
+from flask import render_template, request
 
 from app import app
 from app.models import Vacancy
-from app.forms import LoginForm
 from app.dashboards import create_pie_dashboard, dash_link
-
-@app.route("/")
-@app.route("/index")
-def index():
-    page_text = "Привет!"
-    return render_template("index.html", title="О проекте", page_text=page_text)
-
-@app.route("/keyskills")
-def keyskills():
-    page_text = "Ключевые навыки"
-    return render_template("keyskills.html", title="Ключевые навыки", page_text=page_text)
-
-@app.route("/salary")
-def salary():
-    page_text = "Распределение зарплат"
-    return render_template("salary.html", title="Распределение зарплат", page_text=page_text)
 
 
 def levels_counts(date_from, date_to):
@@ -34,26 +17,49 @@ def levels_counts(date_from, date_to):
         }
     return levels_counts
 
+
+@app.route("/")
+@app.route("/index")
+def index():
+    page_text = "Привет!"
+    return render_template("index.html", title="О проекте", page_text=page_text)
+
+
+@app.route("/keyskills")
+def keyskills():
+    page_text = "Ключевые навыки"
+    return render_template("keyskills.html", title="Ключевые навыки", page_text=page_text)
+
+
+@app.route("/salary")
+def salary():
+    page_text = "Распределение зарплат"
+    return render_template("salary.html", title="Распределение зарплат", page_text=page_text)
+
+
 @app.route("/vacancies", methods=["GET"])
 def vacancies():
-    """При вводе даты передает значения в переменные date_from, date_to."""
-    page_text = "Количество вакансий по уровням"
-
     """
+    При вводе даты передает значения в переменные date_from, date_to.
     Пытаемся получить данные через GET и привести их в формат даты.
     Если не получилось(например пустое поле) подставляем дефолтные значения.
     """
     try:
-        date_from = datetime.strptime(request.args.get("date_from"), '%Y-%m-%d').date()
+        get_date_from = request.args.get("date_from")
+        if get_date_from is None:
+            raise ValueError
+        date_from = datetime.strptime(get_date_from, '%Y-%m-%d').date()
     except ValueError:
         date_from = datetime.strptime('2021-01-01', '%Y-%m-%d').date()
 
     try:
-        date_to = datetime.strptime(request.args.get("date_to"), '%Y-%m-%d').date()
+        get_date_to = request.args.get("date_to")
+        if get_date_to is None:
+            raise ValueError
+        date_to = datetime.strptime(get_date_to, '%Y-%m-%d').date()
     except ValueError:
         date_to = date.today()
 
     image = dash_link(create_pie_dashboard(levels_counts(date_from, date_to)))
 
-    # flash(f"Выбранная дата: c {date_from} до {date_to}.", 'message')
-    return render_template("vacancies.html",title="Количество вакансий по уровням", page_text=page_text, image=image)
+    return render_template("vacancies.html",title="Количество вакансий по уровням", image=image)
