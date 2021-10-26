967eb5058d4e19c09affe917331ac722255679cd
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-10-26 21:38:38 +0300

    Вынес обработку дат в отдельную функцию

--- flask_carcas/app/routes.py
+++ flask_carcas/app/routes.py
@@ -11,12 +11,35 @@ def levels_counts(date_from, date_to):
     """
     Функция делает запрос у БД с фильтрами по дате и уровню
     """
-    levels = ['JUNIOR', 'MIDDLE', 'SENIOR', 'UNDEFINED']
+    levels = ['JUNIOR', 'MIDDLE', 'SENIOR']
     levels_counts = {
         level_name: Vacancy.query.filter(Vacancy.created_at>=date_from).filter(Vacancy.created_at<=date_to).filter_by(level=level_name).count() for level_name in levels
         }
     return levels_counts
 
+
+def get_date(get_date_from, get_date_to):
+    """
+    Поскольку фильтация по дате присутствует на всех страницах,
+    то вынес преобразование результата GET-запроса даты в отдельную функцию.
+    Проверяем входящие данные (не пустые ли) и в зависимости отрезультата
+    подставляем либо дефолтное значение, либо введеную дату
+    """
+    try:
+        if get_date_from is None:
+            raise ValueError
+        date_from = datetime.strptime(get_date_from, '%Y-%m-%d').date()
+    except ValueError:
+        date_from = datetime.strptime('2021-01-01', '%Y-%m-%d').date()
+
+    try:
+        if get_date_to is None:
+            raise ValueError
+        date_to = datetime.strptime(get_date_to, '%Y-%m-%d').date()
+    except ValueError:
+        date_to = date.today()
+
+    return date_from, date_to
 
 @app.route("/")
 @app.route("/index")
@@ -40,26 +63,11 @@ def salary():
 @app.route("/vacancies", methods=["GET"])
 def vacancies():
     """
-    При вводе даты передает значения в переменные date_from, date_to.
-    Пытаемся получить данные через GET и привести их в формат даты.
-    Если не получилось(например пустое поле) подставляем дефолтные значения.
+    Вывод круговой диаграммы со счетчиком вакансий по уровням
     """
-    try:
-        get_date_from = request.args.get("date_from")
-        if get_date_from is None:
-            raise ValueError
-        date_from = datetime.strptime(get_date_from, '%Y-%m-%d').date()
-    except ValueError:
-        date_from = datetime.strptime('2021-01-01', '%Y-%m-%d').date()
+    get_date_from = request.args.get("date_from")
+    get_date_to = request.args.get("date_to")
 
-    try:
-        get_date_to = request.args.get("date_to")
-        if get_date_to is None:
-            raise ValueError
-        date_to = datetime.strptime(get_date_to, '%Y-%m-%d').date()
-    except ValueError:
-        date_to = date.today()
-
-    image = dash_link(create_pie_dashboard(levels_counts(date_from, date_to)))
+    image = dash_link(create_pie_dashboard(levels_counts(get_date(get_date_from, get_date_to))))
 
     return render_template("vacancies.html",title="Количество вакансий по уровням", image=image)
