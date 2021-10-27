734790a6a0f6bfd56820cc1665a4cc470c9d3873
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-10-27 10:51:04 +0300

    Внес правки по замечаниям

--- flask_carcas/db_worker.py
+++ flask_carcas/db_worker.py
@@ -8,7 +8,7 @@ from constants import search_text
 hh = HeadHunterClient()
 
 
-def vacancies_ids(text: str, area_id: int) -> list:
+def merge_vacancies_ids(text: str, area_id: int) -> list:
     """
     Функция для получения максимально возможного
     количества (2000) id вакансий на один запрос.
@@ -19,32 +19,33 @@ def vacancies_ids(text: str, area_id: int) -> list:
         vacancies_ids_on_page, pages = hh.get_vacancies_ids(text, page, area_id)
         vacancies_ids_total.extend(vacancies_ids_on_page)
         if (pages - page) <= 1:
-            return vacancies_ids_total
+            break
+    return vacancies_ids_total
 
 
-def write_to_db(vacancies_id):
-    for id in vacancies_id:
+def write_to_db(vacancies_ids):
+    for vacancy_id in vacancies_ids:
         """
         Что бы не дергать api по каждой вакансии при повторном запуске
         сначала проверим есть ли уже вакансия с hh_id в локальной базе.
         Если нет то будем запрашивать детали вакансии.
         """
-        is_vacancy_add = Vacancy.query.filter_by(hh_id=id).first()
+        is_vacancy_add = Vacancy.query.filter_by(hh_id=vacancy_id).first()
         if is_vacancy_add is None:
-            vacancy_detail = hh.get_vacancy_detail(id)
+            vacancy_detail = hh.get_vacancy_detail(vacancy_id)
             area = Area.insert(vacancy_detail['area_id'], vacancy_detail['area_name'])
 
             employer = Employer.insert(vacancy_detail['employer_id'], vacancy_detail['employer_name'])
 
             vacancy_obj = Vacancy.insert(
-                int(vacancy_detail['hh_id']),
+                vacancy_detail['hh_id'],
                 vacancy_detail['salary_from'],
                 vacancy_detail['salary_to'],
                 vacancy_detail['currency'],
                 vacancy_detail['experience_id'],
                 vacancy_detail['schedule_id'],
                 vacancy_detail['employment_id'],
-                int(area.id),
+                area.id,
                 employer.id,
                 datetime.strptime(vacancy_detail['created_at'], '%Y-%m-%dT%H:%M:%S%z').date(),
                 vacancy_detail['level']
@@ -58,7 +59,7 @@ def worker():
     areas_ids = hh.get_areas_ids()
     for text in search_text:
         for area_id in areas_ids:
-            vacancies_id = vacancies_ids(text, area_id)
+            vacancies_id = merge_vacancies_ids(text, area_id)
             write_to_db(vacancies_id)
 
 
