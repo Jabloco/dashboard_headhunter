99cde2b089f768f4005337d99f52d3aeb75852e1
jabloco <zefir4eg@gmail.com>
2021-11-08 01:03:38 +0300

    поправил tasks

--- flask_carcas/tasks.py
+++ flask_carcas/tasks.py
@@ -4,7 +4,9 @@ from celery.schedules import crontab
 import db_worker
 
 celery_app = Celery('tasks', broker=('redis://localhost:6379/0'))
-
+celery_app.conf.update(
+    timezone='Europe/Moscow'
+)
 @celery_app.task
 def db_fill():
     db_worker.worker()
@@ -12,4 +14,4 @@ def db_fill():
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     """запуск обновления базы каждые 12 часов. в полночь и 12 дня"""
-    sender.add_periodic_task(crontab(minute=0, hour='0,12'), db_fill.s())
+    sender.add_periodic_task(crontab(minute=0, hour='*/12', day_of_week='*'), db_fill.s())
