5c4d36ec5e188f36fb6f90d04a3c970e5fe37af9
jabloco <zefir4eg@gmail.com>
2021-11-09 07:00:30 +0300

    добавил проверку возращаемого значения

--- flask_carcas/db_worker.py
+++ flask_carcas/db_worker.py
@@ -31,28 +31,31 @@ def write_to_db(vacancies_ids):
         Если нет то будем запрашивать детали вакансии.
         """
         is_vacancy_add = Vacancy.query.filter_by(hh_id=vacancy_id).first()
+
         if is_vacancy_add is None:
             vacancy_detail = hh.get_vacancy_detail(vacancy_id)
-            area = Area.insert(vacancy_detail['area_id'], vacancy_detail['area_name'])
 
-            employer = Employer.insert(vacancy_detail['employer_id'], vacancy_detail['employer_name'])
+            if vacancy_detail:
+                area = Area.insert(vacancy_detail['area_id'], vacancy_detail['area_name'])
 
-            vacancy_obj = Vacancy.insert(
-                vacancy_detail['hh_id'],
-                vacancy_detail['salary_from'],
-                vacancy_detail['salary_to'],
-                vacancy_detail['currency'],
-                vacancy_detail['experience_id'],
-                vacancy_detail['schedule_id'],
-                vacancy_detail['employment_id'],
-                area.id,
-                employer.id,
-                datetime.strptime(vacancy_detail['created_at'], '%Y-%m-%dT%H:%M:%S%z').date(),
-                vacancy_detail['level']
-            )
+                employer = Employer.insert(vacancy_detail['employer_id'], vacancy_detail['employer_name'])
 
-            keyskills = [skill['name'] for skill in vacancy_detail['key_skills']]
-            keyskill_vacancy(vacancy_obj, keyskills)
+                vacancy_obj = Vacancy.insert(
+                    vacancy_detail['hh_id'],
+                    vacancy_detail['salary_from'],
+                    vacancy_detail['salary_to'],
+                    vacancy_detail['currency'],
+                    vacancy_detail['experience_id'],
+                    vacancy_detail['schedule_id'],
+                    vacancy_detail['employment_id'],
+                    area.id,
+                    employer.id,
+                    datetime.strptime(vacancy_detail['created_at'], '%Y-%m-%dT%H:%M:%S%z').date(),
+                    vacancy_detail['level']
+                )
+
+                keyskills = [skill['name'] for skill in vacancy_detail['key_skills']]
+                keyskill_vacancy(vacancy_obj, keyskills)
 
 
 def worker():
