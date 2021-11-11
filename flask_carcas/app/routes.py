c9115046284b35864f721f7d55b5a0974703150b
Andrey Butakov <imblackmamba13@yandex.ru>
2021-11-11 10:05:55 +0300

    Поправил условия в отображении даты, поправил ссылку на свой гитхаб

--- flask_carcas/app/routes.py
+++ flask_carcas/app/routes.py
@@ -109,14 +109,16 @@ def keyskills():
 
     image = dash_link(create_keyskills_dashboard(keyskills_count(date_from, date_to, get_skills)))
     
-    if get_date_from == '' and get_date_to == '':
+    date_to_str = date_to.strftime('%d.%m.%Y')
+    date_from_str = date_from.strftime('%d.%m.%Y')
+    if not get_date_from and not get_date_to:
         flash("Данные за все время:")
-    elif get_date_from == '':
-        flash(f"Данные по {date_to.strftime('%d.%m.%Y')}:")
-    elif get_date_to == '':
-        flash(f"Данные с {date_from.strftime('%d.%m.%Y')}:")
+    elif not get_date_from:
+        flash(f"Данные по {date_to_str}:")
+    elif not get_date_to:
+        flash(f"Данные с {date_from_str}:")
     else:
-        flash(f"Данные с {date_from.strftime('%d.%m.%Y')} по {date_to.strftime('%d.%m.%Y')}:")
+        flash(f"Данные с {date_from_str} по {date_to_str}:")
 
     return render_template(
         "keyskills.html",
@@ -128,8 +130,6 @@ def keyskills():
 @hh_app.route("/salary", methods=["GET"])
 def salary():
     page_text = "Распределение зарплат"
-    date_from = request.args.get("date_from")
-    date_to = request.args.get("date_to")
     image = dash_link(create_salary_dashboard(
         create_salaries(Levels.JUNIOR.name),
         create_salaries(Levels.MIDDLE.name),
